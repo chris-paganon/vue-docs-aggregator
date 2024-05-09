7f7cda7da69d1825af7a94fd00537eaa91423e99
chrispaganon <info@chrispaganon.com>
2024-05-09 14:34:19 -0400

    Upload the files to openai vector

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 import json
-from dotenv import load_dotenv, dotenv_values 
-load_dotenv() 
- 
+import datetime
+from dotenv import load_dotenv, dotenv_values
+from openai import OpenAI
+
+load_dotenv()
+
 def build_index():
   docs_index = [
     {
@@ -21,10 +24,10 @@ def build_index():
       for file in files:
         if ".md" in file:
           file_path = dirpath + '/' + file
-          output_file_path = './output/' + file
+          output_file_path = './output/files/' + file
           relative_file_path = dirpath.replace(doc_index["path"], '')
           
-          os.makedirs('./output', exist_ok=True)
+          os.makedirs('./output/files', exist_ok=True)
           os.system(f'cp {file_path} {output_file_path}')
 
           json_file_content.append({
@@ -37,4 +40,20 @@ def build_index():
   json_file.write(json.dumps(json_file_content, indent=2))
   json_file.close()
 
-# build_index()
+def create_file_vector():
+  client = OpenAI(
+    organization=os.getenv("OPENAI_ORG"),
+    api_key=os.getenv("OPENAI_API_KEY")
+  )
+
+  current_time = datetime.datetime.now().strftime("%H:%M:%S")
+  vector_store = client.beta.vector_stores.create(name="VueJS Docs " + current_time)
+  file_paths = [f"./output/files/{file}" for file in os.listdir("./output/files")]
+  file_streams = [open(path, "rb") for path in file_paths]
+ 
+  file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
+    vector_store_id=vector_store.id, files=file_streams
+  )
+
+build_index()
+create_file_vector()
